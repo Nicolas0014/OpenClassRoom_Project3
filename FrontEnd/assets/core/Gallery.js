09f9c6a30ef644e6ffb22e5cf749181c2b138826
Nicolas <charbonnier.nicolas@outlook.fr>
2023-09-04 16:41:21 +0200

    Etape 1.2 finie

--- FrontEnd/assets/core/Gallery.js
+++ FrontEnd/assets/core/Gallery.js
@@ -2,6 +2,7 @@ export default class Gallery{
     constructor(container){
         this.container = container;
         this.display();
+        this.addFilters();
     }
 
     async display(){
@@ -23,6 +24,30 @@ export default class Gallery{
 
             container.appendChild(figure);
         }
+    }
 
+    async addFilters(){
+        let response = await fetch("http://localhost:5678/api/categories")
+        let categories = await response.json();
+        categories.unshift({
+            id:0,
+            name: "Tous"
+        })
+        const container = document.querySelector('.filters');
+        for (let category of categories){
+            const filter = document.createElement("div");
+            filter.innerText = category.name;
+            filter.classList.add('filter');
+            if (category.id === 0){
+                filter.classList.add('active');
+            }
+
+            filter.addEventListener('click', this.onFilterClick.bind(this, category.id))
+            container.appendChild(filter);
+        }
+    }
+
+    onFilterClick(id){
+        console.log(this, id);
     }
 }
