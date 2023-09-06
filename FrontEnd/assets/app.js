9ab3e06b6641a19ce7295ede93010bc161cacec5
Nicolas Charbonnier <charbonnier.nicolas@outlook.fr>
2023-09-06 14:33:39 +0200

    Étape 2.2 : Authentification de l’utilisateur

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,3 +1,15 @@
 import Gallery from "./core/Gallery.js"
+import Login from "./core/Login.js"
 
 new Gallery(document.querySelector('.gallery'));
+new Login(document.querySelector('.formulaire'));
+
+let token = window.localStorage.getItem("token");
+
+if (token) {
+    console.log("Admin");
+} else {
+    console.log("Visiteur simple");
+} 
+
+
---
+++ FrontEnd/assets/core/Login.js
@@ -0,0 +1,55 @@
+export default class Login{
+    constructor(formulaire){
+        this.formulaire = formulaire;
+        this.addListenerConnexion();
+    }
+
+    async addListenerConnexion(){
+        const login = document.querySelector('.formulaire');
+        login.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            let logs = {
+                email : event.target.querySelector("[name=email").value,
+                password : event.target.querySelector("[name=password").value
+            };
+
+            logs = JSON.stringify(logs);
+
+            let response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                body : logs,
+                headers: { "Content-Type": "application/json" }
+            });
+
+            switch (response.status) {
+                case 404 :
+                    this.onIncorrectEmail();
+                    break;
+                case 401 :
+                    this.onIncorrectPassword();
+                    break;
+                case 200 :
+                    this.toHomePage();
+                    const data = await response.json();
+                    const token = data.token;
+                    window.localStorage.setItem("token", token);
+                    break;
+                default :
+                  console.log(`Une erreur inconnue est survenue`);
+              }
+        })
+    }
+
+    toHomePage(){
+        document.location.href="index.html"; 
+    }
+
+    onIncorrectEmail(){
+        alert("Identifiant incorrect. Veuillez réessayer.")
+    }
+
+    onIncorrectPassword(){
+        alert("Mot de passe incorrect. Veuillez réessayer.")
+    }
+
+}
